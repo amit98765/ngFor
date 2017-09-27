33dfe6574ff9404a48eca6b32155db451a7da468
amit98765 <amitkumarmittalakm@gmail.com>
2017-09-27 13:10:47 +1000

    ng Switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1 *ngIf="title; else mycomponent"> 
-    Title is present
-  </h1>
-  <ng-template #mycomponent> This is my component </ng-template>
+    <div [ngSwitch]="titles">
+      <div *ngSwitchCase="'app'">
+        this mathced with app
+      </div>
+      <div *ngSwitchCase="'result'">
+        It matched with result
+      </div>
+    </div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titles = 'app';
+  titles = 'result';
   teams = [{ basketball: 'Indians' }, { basketball: 'chinese' }, { basketball: 'rhinos' }];
 }
