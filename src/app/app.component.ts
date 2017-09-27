b5c2a09312d2212da86accdcde6b4c233be2fcb0
amit98765 <amitkumarmittalakm@gmail.com>
2017-09-27 12:18:36 +1000

    add Index to list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <ul>
+    <li *ngFor="let team of teams; let i = index">
+      {{i}}.{{team.basketball}}
+    </li>
+  </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  teams = [{ basketball: 'Indians' }, { basketball: 'chinese' }, { basketball: 'rhinos' }];
 }
